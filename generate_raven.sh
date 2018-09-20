50825ee3f9868fdd553805071535dd9bfc235ae8
Lincoln Thurlow <lincoln@isi.edu>
2018-09-20 13:25:37 -0700

    initializing raven information

---
+++ generate_raven.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "rvn requires root access - please sudo"
+   exit 1
+fi
+
+if rvn ip server > /dev/null; then
+    echo "Checking output..."
+    response=$(rvn ip server | wc -m)
+    # 8 because .... + 4 characters minimum
+    if [[ "$response" -lt 8 ]]; then
+	echo "Did not return valid ip, check 'rvn deploy/configure'"
+	exit 1
+    fi
+else
+    echo "Unable to run raven, confirm correct directory and 'rvn build'"
+    exit 1
+fi
+
+admin=$(rvn ip server)
+ceph0=$(rvn ip ceph0)
+ceph1=$(rvn ip ceph1)
+ceph2=$(rvn ip ceph2)
+
+echo "" > raven
+echo "# auto generated file: raven" >> raven
+echo "" >> raven
+echo "[deploy]" >> raven
+echo "$ceph0" >> raven
+echo "$ceph1" >> raven
+echo "$ceph2" >> raven
+echo "" >> raven
+echo "[admin]" >> raven
+echo "$admin" >> raven
---
+++ model.js
@@ -0,0 +1,43 @@
+
+cephs = {
+    node: Range(3).map(i => Client('ceph'+i))
+}
+
+switch1 = {
+    'name': 'switch1',
+    'image': 'cumulusvx-3.5',
+    'os': 'linux',
+    'cpu': { 'cores': 1 },
+    'memory': { 'capacity': GB(1) },
+}
+
+ports = {
+    switch1: 1,
+}
+
+// use the raven image for fedora
+server = {
+  name: 'server',
+  image: 'ubuntu-1604',
+  cpu: { cores: 2 },
+  memory: { capacity: GB(4) },
+}
+
+topo = {
+  name: 'sled-basic',
+  nodes: [...cephs.node, server],
+  switches: [switch1],
+  links: [
+    ...cephs.node.map(x => Link(x.name, 0, 'switch1', ports.switch1++)),
+    Link('server', 0, 'switch1', ports.switch1++)
+  ]
+}
+
+function Client(nameIn) {
+    return ceph = {
+      name: nameIn,
+      image: 'ubuntu-1604',
+      cpu: { cores: 2 },
+      memory: { capacity: GB(4) },
+    };
+}
