4fee70d3a56739282368fd26bd8938eff03ea9a6
Lincoln Thurlow <lincoln@isi.edu>
2018-09-24 21:32:14 -0700

    osd's built and working

--- generate_disks.sh
+++ generate_disks.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "requires root access to mount- please sudo"
+   exit 1
+fi
+
 # gigabytes
-disk_size=5
+disk_size=20 # Unable to set partition 2's name to 'ceph journal' > 10gb, recommended 20gb
 ceph_nodes=('ceph0' 'ceph1' 'ceph2' 'ceph3')
 
 for i in "${ceph_nodes[@]}"
