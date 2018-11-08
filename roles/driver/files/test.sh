357b1ec5fccdccc7ef3176fd9ef757bc351ff2c3
Lincoln Thurlow <lincoln@isi.edu>
2018-11-07 21:02:47 -0800

    fix host - everything working

--- roles/driver/files/test.sh
+++ roles/driver/files/test.sh
@@ -1,3 +1,3 @@
 cd /root/go/src/gitlab.com/dcomptb/agents/test/
 
-GOPATH=/root/go GOROOT=/root/gosrc/go CMDR_HOST=commander NODE_HOST=agent go test -count 1 -v
+GOPATH=/root/go GOROOT=/root/gosrc/go CMDR_HOST=commander NODE_HOST=driver go test -count 1 -v
